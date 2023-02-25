19f752f9c4cc9ef0c4e67d92df463cc67243e9f7
Cuong Giang <thaicuong.giang@gmail.com>
2023-02-25 09:49:43 +0700

    refactor ui

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,26 +16,28 @@
 </head>
 
 <body>
-    <div id="dibi" class="font-sans antialiased h-screen flex overflow-hidden bg-gray-200" v-cloak>
-        <div class="flex shrink-0">
-            <div class="flex flex-col w-64">
-                <div class="flex flex-col h-0 flex-1">
-                    <!-- Logo -->
-                    <div class="flex items-center h-16 px-4 bg-gray-900 text-xl text-white font-medium">
-                        <icon-database size="12" fill="#25C4F2"></icon-database>
-                        <div class="ml-2" style="padding-top: 2px;">Dibi</div>
-                    </div>
+    <div id="dibi" v-cloak>
+        <div class="font-sans antialiased h-screen flex overflow-hidden bg-gray-200">
+            <div class="flex shrink-0">
+                <div class="flex flex-col w-64">
+                    <div class="flex flex-col h-0 flex-1">
+                        <!-- Logo -->
+                        <div class="flex items-center h-16 px-4 bg-gray-900 text-xl text-white font-medium">
+                            <icon-database size="12" fill="#25C4F2"></icon-database>
+                            <div class="ml-2" style="padding-top: 2px;">Dibi</div>
+                        </div>
 
-                    <nav-list :tables="{{ json_encode($tables) }}" />
+                        <nav-list :tables="{{ json_encode($tables) }}" />
+                    </div>
                 </div>
             </div>
+
+            <router-view></router-view>
+
+            <!-- Modal Portal -->
+            <portal-target name="modal" multiple>
+            </portal-target>
         </div>
-
-        <router-view></router-view>
-
-        <!-- Modal Portal -->
-        <portal-target name="modal" multiple>
-        </portal-target>
     </div>
 
     <script>
