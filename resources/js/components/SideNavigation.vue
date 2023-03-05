62eb922830c5118c82e9041f5cfe991d2dcf3203
Cuong Giang <thaicuong.giang@gmail.com>
2023-03-05 23:27:09 +0700

    refactor ui

--- resources/js/components/SideNavigation.vue
+++ resources/js/components/SideNavigation.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex-1 flex flex-col overflow-y-auto bg-gray-800">
-        <div class="px-2 py-2">
+        <div class="p-2 sticky top-0">
             <x-input
                 v-model="keyword"
                 type="text"
@@ -9,29 +9,31 @@
             />
         </div>
 
-        <!-- Tables tabs -->
-        <h3
-            class="px-2 mt-4 text-xs leading-4 font-semibold text-gray-500 uppercase tracking-wider"
-        >
-            Tables
-        </h3>
-
-        <nav class="text-sm text-white">
-            <router-link
-                v-for="table in filteredTables"
-                :key="`table-${table.tableName}`"
-                :to="`/tables/${table.tableName}`"
-                active-class="bg-gray-700 rounded-l-full border-r-4 border-blue-500 group mt-1"
-                class="flex w-full pr-6 pl-4 py-2 items-center gap-x-4 hover:bg-gray-700 hover:rounded-l-full"
-                :title="table.tableName"
+        <div class="flex flex-col overflow-y-auto">
+            <!-- Tables tabs -->
+            <h3
+                class="px-2 mt-4 text-xs leading-4 font-semibold text-gray-500 uppercase tracking-wider"
             >
-                <icon-table
-                    size="6"
-                    class="shrink-0"
-                />
-                {{ table.tableName }}
-            </router-link>
-        </nav>
+                Tables
+            </h3>
+
+            <nav class="text-sm text-white">
+                <router-link
+                    v-for="table in filteredTables"
+                    :key="`table-${table.tableName}`"
+                    :to="`/tables/${table.tableName}`"
+                    active-class="bg-gray-700 rounded-l-full border-r-4 border-blue-500 group mt-1"
+                    class="flex w-full pr-6 pl-4 py-2 items-center gap-x-4 hover:bg-gray-700 hover:rounded-l-full"
+                    :title="table.tableName"
+                >
+                    <icon-table
+                        size="6"
+                        class="shrink-0"
+                    />
+                    {{ table.tableName }}
+                </router-link>
+            </nav>
+        </div>
     </div>
 </template>
 
