ece7872d6142cf25236f81e0c323f77bbd4c8131
Cuong Giang <thaicuong.giang@gmail.com>
2023-03-09 08:53:58 +0700

    fix pagination

--- resources/js/views/TableDetails.vue
+++ resources/js/views/TableDetails.vue
@@ -203,7 +203,7 @@
                                     <button
                                         class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 transition duration-150 ease-in-out rounded-l-none disabled:opacity-25"
                                         title="Next page"
-                                        :disabled="records.length < limit || loadingRecords"
+                                        :disabled="records.length <= limit || loadingRecords"
                                         @click.prevent="selectNextPage"
                                     >
                                         <icon-chevron-right size="4" />
